584905b0e8e02cad1593fbcd6361cfe9daec7282
amin <amin.shafiee.d@gmail.com>
2024-06-11 09:39:29 +0330

    fix clients

--- src/views/oauth-clients/update.php
+++ src/views/oauth-clients/update.php
@@ -18,7 +18,8 @@ $this->params['breadcrumbs'][] = Yii::t('openBanking', 'Update');
     <div class="card-header"><h3><?= Html::encode($this->title) ?></h3></div>
     <div class="card-body">
         <?= $this->render('_form', [
-        'model' => $model,
+            'model' => $model,
+            'platform' => $model->client_id
         ]) ?>
     </div>
 </div>
