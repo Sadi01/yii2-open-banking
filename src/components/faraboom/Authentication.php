0deec90c8b36ab4c832858b3d9aa1ed3d5d3d9e3
Sadegh Shafii <Sadshafiei.01@gmail.com>
2024-06-18 11:10:14 +0000

    Update Authentication.php

--- src/components/faraboom/Authentication.php
+++ src/components/faraboom/Authentication.php
@@ -35,9 +35,9 @@ class Authentication extends BaseAuthentication
             $headers['Authorization'] = 'Basic ' . base64_encode($client->app_key . ':' . $client->app_secret);
             $headers['Bank-Id'] = $client->bank_id;
             $headers['CLIENT-DEVICE-ID'] = $client->client_device_id;
-            $headers['CLIENT-IP-ADDRESS'] = Yii::$app->request->userIP;
+            $headers['CLIENT-IP-ADDRESS'] = Yii::$app->request->userIP ?? $client->client_device_id;
             $headers['CLIENT-PLATFORM-TYPE'] = $client->client_platform_type;
-            $headers['CLIENT-USER-AGENT'] = Yii::$app->request->userAgent;
+            $headers['CLIENT-USER-AGENT'] = Yii::$app->request->userAgent ?? '';
             $headers['CLIENT-USER-ID'] = $client->client_user_id;
             $headers['Content-Type'] = Client::FORMAT_URLENCODED;
             $headers['Device-Id'] = $client->device_id;
