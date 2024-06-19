f34c8bd5dca82b48f2ae40d6597ecd6886123f07
amin <amin.shafiee.d@gmail.com>
2024-06-19 08:39:24 +0330

    enh Authentication

--- src/components/faraboom/Authentication.php
+++ src/components/faraboom/Authentication.php
@@ -93,11 +93,11 @@ class Authentication extends BaseAuthentication
 
         $headers['Authorization'] = $client->authorization;
         $headers['Device-Id'] = $client->device_id;
-        $headers['CLIENT-IP-ADDRESS'] = Yii::$app->request->userIP;
+        $headers['CLIENT-IP-ADDRESS'] = Yii::$app->request->userIP ?? $client->client_device_id;
         $headers['CLIENT-PLATFORM-TYPE'] = $client->client_platform_type;
         $headers['CLIENT-DEVICE-ID'] = $client->client_device_id;
         $headers['CLIENT-USER-ID'] = $client->client_user_id;
-        $headers['CLIENT-USER-AGENT'] = Yii::$app->request->userAgent;
+        $headers['CLIENT-USER-AGENT'] = Yii::$app->request->userAgent ?? '';
 
         $response = Yii::$app->apiClient->post($url, $param, $headers);
         if ($response['status'] === 200) {
